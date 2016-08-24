8a00139eaa5da5bf6bb0f437693d0a417a7833dc
The Nola 11 Corporation <thenola11@gmail.com>
2016-08-24 18:46:41 -0500

    Update base.js

--- mandala/resources/base.js
+++ mandala/resources/base.js
@@ -1,33 +1,38 @@
+(function() {
+"use strict";
+
 // Base Namespace
-var m;
+var m = {};
 
 // Data Model
-m.odel = 
-{
-  encryption: "",
-  ip: 00.000000,
-  key:,
-  name: ""
+m.odel = {
+  encryption: "MD5",
+  ip: "00.000000",
+  key: "69f3752989a4188b52b7e169374ca82a",
+  name: "Server Name"
 };
 
 // Library / Handler
-m.andala = 
-{
+m.andala = {
   functions:
   {
     init: function ()
     {
       // This is your data binder...
-      // m.andala.functions.add (this.add) creates data objects based on the model above and inflates. 
+      // m.andala.functions.add (this.add) creates data objects based on the model above and inflates.
+
+      return false;
     },
     add: function ( encryption, ip, key, name )
     {
       m.andala.data.push({encryption:encryption, ip:ip, key:key, name:name});
     }
   },
-  data: [];
-}
+  data: []
+};
 
 m.andala.functions.init();
 
 return m;
+
+})();
