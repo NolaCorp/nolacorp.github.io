822e7dd63ab45d5bd63a1b61e22e3822abf88bd6
The Nola 11 Corporation <thenola11@gmail.com>
2016-08-24 18:12:28 -0500

    Create base.js

---
+++ mandala/resources/base.js
@@ -0,0 +1,20 @@
+// Base Namespace
+var m;
+
+// Data Model
+m.odel = 
+{
+  ip: 00.000000,
+  name: "",
+  md5: ""
+};
+
+// Library / Handler
+m.andala = 
+{
+  data:
+  [
+  ]
+}
+
+return m.andala;
